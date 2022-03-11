ea9cd22ebc5b24e019deacec7ec4e7543c1c9695
Danilova-Viktoria <danilovaviktorija5@gmail.com>
2022-03-11 18:32:27 +0300

    Добавление таблицы

---
+++ script.js
@@ -0,0 +1,28 @@
+const table = document.querySelector("table");
+const headers = [];
+
+fetch("https://sb-cats.herokuapp.com/api/show")
+    .then(res => res.json())
+    .then(dataJSON => {
+        if (dataJSON.mesage ==="ok") {
+            let innerHTML = "";
+
+            innerHTML += "<tr>";
+            for (key of Object.keys(dataJSON.data[0])) {
+                if (key !== "__v" && key !== "_id"){
+                    headers.push(key)
+                    innerHTML +=`<th>${key}</th>`
+                }
+            }
+            innerHTML += "</tr>"
+
+            dataJSON.data.forEach(cat => {
+                innerHTML += "</tr>"
+                headers.forEach(title =>
+                    innerHTML += `<td>${cat[title]}</td>`
+                    )
+                    innerHTML += "</tr>"
+            })
+            table.innerHTML = innerHTML;
+        }
+    }) 
